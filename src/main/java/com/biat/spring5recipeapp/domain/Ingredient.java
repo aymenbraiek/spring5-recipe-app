658c511a3db6a95461a6e3e1de963e403e701553
aymenbraiek <aymen.braiek@esprit.tn>
2021-04-17 16:44:35 +0100

    adding OneToMany JPA  Relationship -closes #3

---
+++ src/main/java/com/biat/spring5recipeapp/domain/Ingredient.java
@@ -0,0 +1,25 @@
+package com.biat.spring5recipeapp.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Ingredient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    private BigDecimal amount;
+    @ManyToOne
+    private Recipe recipe;
+}
--- src/main/java/com/biat/spring5recipeapp/domain/Recipe.java
+++ src/main/java/com/biat/spring5recipeapp/domain/Recipe.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 @NoArgsConstructor
@@ -24,6 +25,8 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
     //for a large Object Storage
     @Lob
     private Byte[] image;
